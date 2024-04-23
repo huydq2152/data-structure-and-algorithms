88c834874559eb9e2304cc3aea5b7c16e4f53bd2
huydq2152 <quanghuy2152@gmail.com>
2024-04-23 22:12:18 +0700

    solve problem leedcode 75,647

---
+++ LeedCode/Problem_075.cs
@@ -0,0 +1,34 @@
+﻿namespace LeedCode;
+
+public class Problem_075
+{
+    public void SortColors(int[] nums)
+    {
+        int numsLength = nums.Length;
+        int left = 0;
+        int right = numsLength - 1;
+        int current = 0;
+        while (current <= right)
+        {
+            if (nums[current] == 0)
+            {
+                int temp = nums[left];
+                nums[left] = 0;
+                nums[current] = temp;
+                left++;
+                current++;
+            }
+            else if (nums[current] == 2)
+            {
+                int temp = nums[right];
+                nums[right] = 2;
+                nums[current] = temp;
+                right--;
+            }
+            else
+            {
+                current++;
+            }
+        }
+    }
+}
---
+++ LeedCode/Problem_647.cs
@@ -0,0 +1,30 @@
+﻿namespace LeedCode;
+
+public class Problem_647
+{
+    public int CountSubstrings(string s)
+    {
+        int result = 0;
+        int sLength = s.Length;
+        for (int i = 0; i < sLength; i++)
+        {
+            result += CountPalindrome(s, i, i) + CountPalindrome(s, i, i + 1);
+        }
+
+        return result;
+    }
+
+    public int CountPalindrome(string s, int left, int right)
+    {
+        int sLength = s.Length;
+        int result = 0;
+        while (left >= 0 && right < sLength && s[left] == s[right])
+        {
+            left--;
+            right++;
+            result++;
+        }
+
+        return result;
+    }
+}
