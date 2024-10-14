852ead0390ca13f56c7b4a66461aa10036a7af6d
huydq2152 <quanghuy2152@gmail.com>
2024-10-14 23:11:52 +0700

    add pb_187

---
+++ LeedCode/Problem_187.cs
@@ -0,0 +1,35 @@
+﻿namespace LeedCode;
+
+public class Problem_187
+{
+    public IList<string> FindRepeatedDnaSequences(string s) {
+        List<string> result = new List<string>();
+        int sLength = s.Length;
+        if (sLength < 10)
+        {
+            return result;
+        }
+
+        Dictionary<string, int> dictionary = new Dictionary<string, int>();
+        
+        for (int i = 0; i + 9 < sLength; i++)
+        {
+            string temp = s.Substring(i, 10);
+            if (dictionary.TryGetValue(temp, out int appearCount))
+            {
+                if (appearCount == 1)
+                {
+                    result.Add(temp);
+                }
+
+                dictionary[temp]++;
+            }
+            else
+            {
+                dictionary.Add(temp, 1);
+            }
+        }
+
+        return result;
+    }
+}
