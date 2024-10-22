e275247b522552db4356af4d8dded6c9c6aab5c6
huydq2152 <quanghuy2152@gmail.com>
2024-10-22 23:44:34 +0700

    update code

---
+++ LeedCode/Array/Precomputation/Problem_303.cs
@@ -0,0 +1,21 @@
+﻿namespace LeedCode;
+
+public class NumArray
+{
+    private int[] prefixSum;
+
+    public NumArray(int[] nums)
+    {
+        int numsLength = nums.Length;
+        prefixSum = new int[numsLength + 1];
+        for (int i = 0; i < numsLength; i++)
+        {
+            prefixSum[i + 1] = prefixSum[i] + nums[i];
+        }
+    }
+
+    public int SumRange(int left, int right)
+    {
+        return prefixSum[right + 1] - prefixSum[left];
+    }
+}
---
+++ LeedCode/Array/Precomputation/Problem_724.cs
@@ -0,0 +1,21 @@
+﻿namespace LeedCode;
+
+public class Problem_724
+{
+    public int PivotIndex(int[] nums) {
+        int sumRight = nums.Sum();
+        int sumLeft = 0;
+    
+        for (int i = 0; i < nums.Length; i++) {
+            sumRight -= nums[i];
+      
+            if (sumLeft == sumRight) {
+                return i;
+            }
+        
+            sumLeft += nums[i];
+        }
+    
+        return -1;
+    }
+}
